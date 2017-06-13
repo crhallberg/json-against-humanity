9d4fa48fa4d85238bbbcfb1bc8a6e4bfb59372ad
Chris Hallberg <crhallberg@gmail.com>
2017-06-13 00:59:49 +0000

    Optimize fulllist.py

--- fulllist.py
+++ fulllist.py
@@ -1,19 +1,18 @@
 import os
 
 blackCards = []
+whiteCards = []
 for deckDir in os.listdir('src/'):
     with open('src/' + deckDir + '/black.md.txt') as f:
         cards = ["%s (%s)" % (x.strip(), deckDir) for x in f.readlines()]
         blackCards.extend(cards)
-uniqueBlack = sorted(blackCards)
-with open('list-black.txt', 'w') as lb:
-    lb.write("\n".join(uniqueBlack))
-
-whiteCards = []
-for deckDir in os.listdir('src/'):
     with open('src/' + deckDir + '/white.md.txt') as f:
         cards = ["%s (%s)" % (x.strip(), deckDir) for x in f.readlines()]
         whiteCards.extend(cards)
+
+uniqueBlack = sorted(blackCards)
 uniqueWhite = sorted(whiteCards)
+with open('list-black.txt', 'w') as lb:
+    lb.write("\n".join(uniqueBlack))
 with open('list-white.txt', 'w') as lb:
     lb.write("\n".join(uniqueWhite))
