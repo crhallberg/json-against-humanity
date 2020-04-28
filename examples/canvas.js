9c3e1aa5af9441b024501fd2cb668e305ce2935f
Chris Hallberg <crhallberg@gmail.com>
2020-04-28 09:59:29 -0400

    Example improvements and cleanup.

--- examples/canvas.js
+++ examples/canvas.js
@@ -6,23 +6,38 @@ function updateCard(card) {
   card.x += (card.tx - card.x) / card.steps;
   card.y += (card.ty - card.y) / card.steps;
   card.rot += (card.trot - card.rot) / card.steps;
+  // Ignore the Achilles Paradox at the end
   if (Math.abs(card.tx - card.x) < 2) {
     card.atRest = true;
   }
 }
+
+const cardWidth = 200,
+  cardHeight = 300,
+  cardPadding = 20,
+  fontSize = 18;
+const cardOffsetX = cardWidth / 2,
+  cardOffsetY = cardHeight / 2,
+  lineHeight = fontSize * 1.15;
 function drawCard(card) {
   c.save();
   c.translate(card.x, card.y);
   c.rotate(card.rot);
   c.fillStyle = card.black ? "#000" : "#fff";
-  c.fillRect(-100, -100, 200, 300);
+  c.fillRect(-cardOffsetX, -cardOffsetY, cardWidth, cardHeight);
   // Text
   c.fillStyle = card.black ? "#fff" : "#000";
   for (let i = 0; i < card.text.length; i++) {
-    c.fillText(card.text[i], -80, -80 + 22 * i);
+    c.fillText(
+      card.text[i],
+      -cardOffsetX + cardPadding,
+      -cardOffsetY + cardPadding + lineHeight * i
+    );
   }
   c.restore();
 }
+
+// Split the text into lines that will fit on the card
 function wrap(text) {
   let words = text.split(" ");
   let lines = [];
@@ -31,7 +46,7 @@ function wrap(text) {
   for (let i = 1; i < words.length; i++) {
     curr += " " + words[i];
     let tm = c.measureText(curr);
-    if (tm.width > 155) {
+    if (tm.width > cardWidth - cardPadding * 2 - 5) {
       lines.push(prev);
       curr = words[i];
     }
@@ -40,6 +55,13 @@ function wrap(text) {
   lines.push(curr);
   return lines;
 }
+
+/**
+ * Make a new card object
+ *
+ * Starts off the leftside of the screen,
+ * should pass thru the button.
+ */
 function makeCard(card, cx, cy) {
   let tx = (Math.random() * 0.7 + 0.3) * a.width;
   let ty = Math.random() * a.height;
@@ -60,7 +82,7 @@ function makeCard(card, cx, cy) {
 
 let loadedPacks = {};
 function shuffle(arr) {
-  arr.sort(() => Math.random() * 2 - 1);
+  arr.sort((a, b) => Math.random() * 2 - 1); // Random number instead of comparison
 }
 function addCards(btn) {
   let abbr = btn.dataset.pack;
@@ -138,7 +160,7 @@ function initCanvas() {
   c = a.getContext("2d");
   c.textAlign = "left";
   c.textBaseline = "top";
-  c.font = "500 18px Inter";
+  c.font = `500 ${fontSize}px Inter`;
 }
 function update() {
   for (let i = 0; i < cardSprites.length; i++) {
