29dab0c20ed0ed78bdd3b4e7f715794f567b1798
Chris Hallberg <chris.hallberg@hey.com>
2021-10-09 20:40:43 -0400

    Add prettier formatting.

--- tools/update-from-sheets.js
+++ tools/update-from-sheets.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { google } = require("googleapis");
 const { nanoid } = require("nanoid");
 const {
-  replace: { exoticChars: replaceExoticChars },
+    replace: { exoticChars: replaceExoticChars },
 } = require("clean-text-utils");
 const readline = require("readline");
 
@@ -15,10 +15,10 @@ const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
 fs.readFile("credentials.json", (err, content) => {
-  if (err) return console.log("Error loading client secret file:", err);
-  console.log("connecting to Google Sheets...");
-  // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), saveCardsToJSON);
+    if (err) return console.log("Error loading client secret file:", err);
+    console.log("connecting to Google Sheets...");
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), saveCardsToJSON);
 });
 
 /**
@@ -28,19 +28,19 @@ fs.readFile("credentials.json", (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+    );
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getNewToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
 }
 
 /**
@@ -50,65 +50,65 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES,
-  });
-  console.log("Authorize this app by visiting this url:", authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question("Enter the code from that page here: ", (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err)
-        return console.error(
-          "Error while trying to retrieve access token",
-          err
-        );
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log("Token stored to", TOKEN_PATH);
-      });
-      callback(oAuth2Client);
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES,
     });
-  });
+    console.log("Authorize this app by visiting this url:", authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question("Enter the code from that page here: ", (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err)
+                return console.error(
+                    "Error while trying to retrieve access token",
+                    err
+                );
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log("Token stored to", TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
 }
 
 function normalizeName(name) {
-  return name.replace("CAH :", "CAH:").trim();
+    return name.replace("CAH :", "CAH:").trim();
 }
 
 let packMap = {};
 function rangeToDeck({ values }) {
-  let header = values.shift();
-  if (header[0] == "Prompt Cards") {
-    return values.map((row) => {
-      let name = normalizeName(row[2]);
-      if (!packMap[name]) {
-        packMap[name] = {
-          id: nanoid(4),
-          official: !!row[3].match("CAH"),
-        };
-      }
-      let picks = row[0].match(/_+/g);
-      return [
-        packMap[name].id,
-        replaceExoticChars(row[0].replace(/_+/g, "_")),
-        picks ? picks.length : row[0] == "Make a haiku." ? 3 : 1,
-      ];
-    });
-  }
-  return values.map((row) => {
-    let name = normalizeName(row[1]);
-    if (!packMap[name]) {
-      packMap[name] = { id: nanoid(4), official: !!name.match("CAH") };
+    let header = values.shift();
+    if (header[0] == "Prompt Cards") {
+        return values.map((row) => {
+            let name = normalizeName(row[2]);
+            if (!packMap[name]) {
+                packMap[name] = {
+                    id: nanoid(4),
+                    official: !!row[3].match("CAH"),
+                };
+            }
+            let picks = row[0].match(/_+/g);
+            return [
+                packMap[name].id,
+                replaceExoticChars(row[0].replace(/_+/g, "_")),
+                picks ? picks.length : row[0] == "Make a haiku." ? 3 : 1,
+            ];
+        });
     }
-    return [packMap[name].id, replaceExoticChars(row[0])];
-  });
+    return values.map((row) => {
+        let name = normalizeName(row[1]);
+        if (!packMap[name]) {
+            packMap[name] = { id: nanoid(4), official: !!name.match("CAH") };
+        }
+        return [packMap[name].id, replaceExoticChars(row[0])];
+    });
 }
 
 /**
@@ -117,63 +117,61 @@ function rangeToDeck({ values }) {
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
 function saveCardsToJSON(auth) {
-  console.log("getting ranges...");
-  const sheets = google.sheets({ version: "v4", auth });
-  sheets.spreadsheets.values.batchGet(
-    {
-      spreadsheetId: "1lsy7lIwBe-DWOi2PALZPf5DgXHx9MEvKfRw1GaWQkzg",
-      ranges: ["Master Cards List!A:E", "Master Cards List!G:J"],
-    },
-    (err, ranges) => {
-      if (err) return console.log("The API returned an error: " + err);
-      console.log("parsing ranges...");
-      let cards = ranges.data.valueRanges.map(rangeToDeck).flat();
+    console.log("getting ranges...");
+    const sheets = google.sheets({ version: "v4", auth });
+    sheets.spreadsheets.values.batchGet(
+        {
+            spreadsheetId: "1lsy7lIwBe-DWOi2PALZPf5DgXHx9MEvKfRw1GaWQkzg",
+            ranges: ["Master Cards List!A:E", "Master Cards List!G:J"],
+        },
+        (err, ranges) => {
+            if (err) return console.log("The API returned an error: " + err);
+            console.log("parsing ranges...");
+            let cards = ranges.data.valueRanges.map(rangeToDeck).flat();
 
-      console.log("separating...");
-      let white = [];
-      let whiteSet = new Set();
-      let black = [];
-      let blackSet = new Set();
-      let packs = {};
-      for (let name in packMap) {
-        let pack = packMap[name];
-        packs[pack.id] = {
-          name,
-          white: [],
-          black: [],
-          official: pack.official,
-        };
-      }
+            console.log("separating...");
+            let packs = {};
+            for (let name in packMap) {
+                let pack = packMap[name];
+                packs[pack.id] = {
+                    name,
+                    white: [],
+                    black: [],
+                    official: pack.official,
+                };
+            }
 
-      for (let card of cards) {
-        if (!card[1]) {
-          continue;
+            let white = [];
+            let black = [];
+            let blackSet = new Set();
+            let whiteSet = new Set();
+            for (let card of cards) {
+                if (!card[1]) {
+                    continue;
+                }
+                if (card.length > 2) {
+                    if (!blackSet.has(card[1])) {
+                        packs[card[0]].black.push(black.length);
+                        black.push({
+                            text: card[1],
+                            pick: card[2],
+                        });
+                        blackSet.add(card[1]);
+                    }
+                } else {
+                    if (!whiteSet.has(card[1])) {
+                        packs[card[0]].white.push(white.length);
+                        white.push(card[1].trim());
+                        whiteSet.add(card[1]);
+                    }
+                }
+            }
+
+            console.log(`saving... (${white.length} white, ${black.length} black)`);
+            fs.writeFileSync(
+                "../cah-all-compact.json",
+                JSON.stringify({ white, black, packs: Object.values(packs) })
+            );
         }
-        if (card.length > 2) {
-          packs[card[0]].black.push(black.length);
-          if (!blackSet.has(card[1])) {
-            blackSet.add(card[1]);
-            black.push({
-              text: card[1],
-              pick: card[2],
-            });
-          }
-        } else {
-          packs[card[0]].white.push(white.length);
-          if (!whiteSet.has(card[1])) {
-            whiteSet.add(card[1]);
-            white.push(
-              card[1].replace(/^[\s\uFEFF\xA0\|]+|[\s\uFEFF\xA0\|]+$/g, "")
-            ); // trim
-          }
-        }
-      }
-
-      console.log(`saving... (${white.length} white, ${black.length} black)`);
-      fs.writeFileSync(
-        "../cah-all-compact.json",
-        JSON.stringify({ white, black, packs: Object.values(packs) })
-      );
-    }
-  );
+    );
 }
